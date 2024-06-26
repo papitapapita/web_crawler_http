8d539e397688f1fe31ca79ad196cfa8f9f0ed950
papita <many.d.r.o.2001@hotmail.com>
2024-06-25 22:11:31 -0500

    initial code of the main file

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+function main(){
+    try{
+        if(process.argv.length < 3){
+            throw Error('No website provided');
+        }else if(process.argv.length > 3){
+            throw Error('Too many arguments');
+        }
+
+        console.log(`The website to crawl is ${process.argv[2]}`);
+    }catch(err){
+        console.error(`There was a problem: ${err.message}`);
+        process.exit(1);
+    }
+}
+
+main();
